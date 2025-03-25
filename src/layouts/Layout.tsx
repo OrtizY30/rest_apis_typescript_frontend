02f7f810bc6e5cfb072611b3f54ab8fdd1ce38d9
Ysreimer Ortiz <ysreimer@gmail.com>
2025-03-25 01:14:47 -0500

    firt commit

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
     <div className="bg-slate-100 min-h-screen">
       <header className="bg-slate-800">
         <div className="mx-auto max-w-6xl py-10">
-          <h1 className="text-4xl font-extrabold text-white">Administtrador de Productos</h1>
+          <h1 className="text-4xl font-extrabold text-white">Administrador de Productos</h1>
         </div>
       </header>
 
